0555c629269745aa3a7638dad4fe7a55b76f3562
MariosPitsali <marios.pitsali@gmail.com>
2017-07-09 22:41:52 +0300

    # added

--- find_syllables.py
+++ find_syllables.py
@@ -7,12 +7,17 @@ parser.add_argument("word", help="Word to find syllables")
 
 args = parser.parse_args()
 
+#parsing word from terminal
+word_input = args.word
 
-word = args.word
-wordz = requests.get("http://www.dictionary.com/browse/%s" %(word))
-leword = BeautifulSoup(wordz.content, "html.parser")
-syl = leword.find_all("span", class_="me")[0].attrs['data-syllable'].split('·')
+#getting html code from dictionary.com
+word = requests.get("http://www.dictionary.com/browse/%s" %(word_input))
+html_doc = BeautifulSoup(word.content, "html.parser")
 
-print(len(syl), "syllables in word",  word)
+#fidning html tags to get syllables
+syl = html_doc.find_all("span", class_="me")[0].attrs['data-syllable'].split('·')
+
+#printing each syllable and syllable count
+print(len(syl), "syllables in word",  word_input)
 for syllable in syl:
   print ("Syllable:", syllable, "\n")
